b8eb3d52b06f6909dff5ed211c11854e2bbc3259
SabineZilde <sabine.zilde@protonmail.ch>
2021-05-13 21:34:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ const rl = readline.createInterface({
 })
 
 rl.question('Please enter your input. ', function (answer) {
-    const cleanedUpStirng = answer.toLowerCase().replace(/ /g, ''); 
-// Turns parameter in lower case and remoces spaces (One Two = onetwo)
+    const cleanedUpStirng = answer.toLowerCase().replace(/ /g, ''); // Turns parameter in lower case and removes spaces (One Two = onetwo)
     const reverseString = cleanedUpStirng.split('').reverse().join('');
-    if (reverseString == cleanedUpStirng) {
+    if (reverseString === cleanedUpStirng) {
         console.log(`${reverseString} is a palindrome`);
     } else {
         console.log(`${reverseString} is not a palindrome`);
