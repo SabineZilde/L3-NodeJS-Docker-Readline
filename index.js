e313e3c7840e320715a282512c5b381565f6bccc
SabineZilde <sabine.zilde@protonmail.ch>
2021-05-13 21:29:23 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+rl.question('Please enter your input. ', function (answer) {
+    const cleanedUpStirng = answer.toLowerCase().replace(/ /g, ''); 
+// Turns parameter in lower case and remoces spaces (One Two = onetwo)
+    const reverseString = cleanedUpStirng.split('').reverse().join('');
+    if (reverseString == cleanedUpStirng) {
+        console.log(`${reverseString} is a palindrom`);
+    } else {
+        console.log(`${reverseString} is not a palindrom`);
+    }
+    rl.close();
+})
